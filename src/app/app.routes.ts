15bcc4d1e245ff27e7fd8f66482fd7ee125de134
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-09-21 14:59:04 -0300

    Added default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CatalogComponent } from './components/catalog/catalog.component';
 import { WorldMapComponent } from './components/world-map/world-map.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
     { path: 'world-map', component: WorldMapComponent }
