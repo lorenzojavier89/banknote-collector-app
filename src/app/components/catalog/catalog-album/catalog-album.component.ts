74c2f7e8d6a63040f6d7dc15cf589907df02bf4b
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-09-21 20:02:19 -0300

    Added Album redirection

--- src/app/components/catalog/catalog-album/catalog-album.component.ts
+++ src/app/components/catalog/catalog-album/catalog-album.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CatalogService } from '../../../services/catalog.service';
 import { Banknote } from '../../../models/banknote.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-catalog-album',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './catalog-album.component.html',
   styleUrl: './catalog-album.component.scss'
 })
