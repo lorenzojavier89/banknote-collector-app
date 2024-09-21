9dfddd6ffb38d6066a312ddd4286c330e8c51845
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-09-21 14:44:08 -0300

    Added initial routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { WorldMapComponent } from './components/world-map/world-map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'catalog', component: CatalogComponent },
+    { path: 'world-map', component: WorldMapComponent }
+];
