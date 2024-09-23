65a310548974d443d2bd5bc07803f84d2ec9a8f0
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-09-22 22:15:54 -0300

    Added ComponentInpuBinding

--- src/app/components/catalog/banknote/banknote.component.ts
+++ src/app/components/catalog/banknote/banknote.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-banknote',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './banknote.component.scss'
 })
 export class BanknoteComponent {
-
+  id = input.required<string>();
 }
--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Banknote } from '../models/banknote.model';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -12,4 +12,10 @@ export class CatalogService {
 
   private banknotes$ = this.http.get<Banknote[]>(this.jsonUrl);
   banknotes = toSignal(this.banknotes$, { initialValue: [] });
+  selectedBanknote = signal<Banknote | undefined>(undefined);
+
+  setSelectedBanknote(id: string) {
+    const foundBanknote = this.banknotes().find(x => x.id === id); 
+    this.selectedBanknote.set(foundBanknote);
+  }
 }
