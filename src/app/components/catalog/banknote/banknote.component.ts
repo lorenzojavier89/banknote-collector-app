fc67e5af8870e6de2dac44745c40004a7e067f81
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-09-22 22:28:06 -0300

    Display Banknote (json format)

--- src/app/components/catalog/banknote/banknote.component.ts
+++ src/app/components/catalog/banknote/banknote.component.ts
@@ -1,12 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, inject, input, OnInit } from '@angular/core';
+import { CatalogService } from '../../../services/catalog.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-banknote',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './banknote.component.html',
   styleUrl: './banknote.component.scss'
 })
-export class BanknoteComponent {
+export class BanknoteComponent implements OnInit {
   id = input.required<string>();
+  private catalogService: CatalogService = inject(CatalogService);
+
+  selectedBanknote = this.catalogService.selectedBanknote;
+
+  ngOnInit(): void {
+    this.catalogService.setSelectedBanknote(this.id());
+  }
 }
