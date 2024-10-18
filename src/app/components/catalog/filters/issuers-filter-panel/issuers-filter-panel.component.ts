22c448cce9b504ace4a22a0396d736966826ac40
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-10-18 10:09:40 -0300

    clean up filtersComponent html

---
+++ src/app/components/catalog/filters/issuers-filter-panel/issuers-filter-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issuers-filter-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './issuers-filter-panel.component.html',
+  styleUrl: './issuers-filter-panel.component.scss'
+})
+export class IssuersFilterPanelComponent {
+
+}
---
+++ src/app/components/catalog/filters/regions-filter-panel/regions-filter-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regions-filter-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './regions-filter-panel.component.html',
+  styleUrl: './regions-filter-panel.component.scss'
+})
+export class RegionsFilterPanelComponent {
+
+}
