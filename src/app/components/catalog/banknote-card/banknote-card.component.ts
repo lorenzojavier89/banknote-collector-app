228d9dab18256f4dc63fb3e2c43666565ec455d3
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-10-22 20:47:13 -0300

    Implement Angular animation to fadeIn

--- src/app/components/catalog/banknote-card/banknote-card.component.ts
+++ src/app/components/catalog/banknote-card/banknote-card.component.ts
@@ -1,13 +1,20 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Banknote } from '../../../models/banknote.model';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-banknote-card',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './banknote-card.component.html',
-  styleUrl: './banknote-card.component.scss'
+  styleUrl: './banknote-card.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [style({opacity: 0}), animate('1s', style({opacity: 1}))]),
+      transition(':leave', [animate('1s', style({opacity: 0}))]),
+    ]),
+  ]
 })
 export class BanknoteCardComponent {
   banknote = input.required<Banknote>();
