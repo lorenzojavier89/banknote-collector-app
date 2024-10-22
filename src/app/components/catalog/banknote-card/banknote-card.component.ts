7d968c77bca15d62c80225ba3bacde595c2cace0
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-10-22 19:36:18 -0300

    Refactor. Added banknoteCardComponent

---
+++ src/app/components/catalog/banknote-card/banknote-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Banknote } from '../../../models/banknote.model';
+
+@Component({
+  selector: 'app-banknote-card',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './banknote-card.component.html',
+  styleUrl: './banknote-card.component.scss'
+})
+export class BanknoteCardComponent {
+  banknote = input.required<Banknote>();
+}
--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,12 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { FiltersComponent } from "./filters/filters.component";
 import { FiltersService } from '../../services/filters.service';
+import { BanknoteCardComponent } from './banknote-card/banknote-card.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [RouterLink, FiltersComponent],
+  imports: [BanknoteCardComponent, FiltersComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
 })
