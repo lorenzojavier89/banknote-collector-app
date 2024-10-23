3c86f2270ac265a160563d2bda599d8fad697916
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-10-22 21:39:56 -0300

    Implement animations in Catalog component

--- src/app/components/catalog/banknote-card/banknote-card.component.ts
+++ src/app/components/catalog/banknote-card/banknote-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Banknote } from '../../../models/banknote.model';
-import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-banknote-card',
@@ -9,11 +8,6 @@ import { trigger, transition, style, animate } from '@angular/animations';
   imports: [RouterLink],
   templateUrl: './banknote-card.component.html',
   styleUrl: './banknote-card.component.scss',
-  animations: [
-    trigger('fadeIn', [
-      transition(':enter', [style({opacity: 0}), animate('1s', style({opacity: 1}))]),
-    ]),
-  ]
 })
 export class BanknoteCardComponent {
   banknote = input.required<Banknote>();
--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -2,13 +2,21 @@ import { Component, computed, inject } from '@angular/core';
 import { FiltersComponent } from "./filters/filters.component";
 import { FiltersService } from '../../services/filters.service';
 import { BanknoteCardComponent } from './banknote-card/banknote-card.component';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [BanknoteCardComponent, FiltersComponent],
+  imports: [BanknoteCardComponent, FiltersComponent, NgFor],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [style({opacity: 0}), animate('1s', style({opacity: 1}))]),
+      transition(':leave', [animate('250ms', style({opacity: 0}))]),
+    ]),
+  ]
 })
 export class CatalogComponent {
   private filtersService: FiltersService = inject(FiltersService);
