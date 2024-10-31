bd86e025ed5fd08a7048131625ff8ea4f72b9ec9
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-10-31 20:46:54 -0300

    Improved header styles with MatCard component

--- src/app/components/catalog/catalog-header/catalog-header.component.ts
+++ src/app/components/catalog/catalog-header/catalog-header.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, inject } from '@angular/core';
 import { FiltersService } from '../../../services/filters.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-catalog-header',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './catalog-header.component.html',
   styleUrl: './catalog-header.component.scss'
 })
