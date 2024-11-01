ed784a5f375ca14646ec468647aadd89a781ea48
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-11-01 16:31:14 -0300

    Added region and subregion badges

--- src/app/components/catalog/catalog-header/catalog-header.component.ts
+++ src/app/components/catalog/catalog-header/catalog-header.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
 import { FiltersService } from '../../../services/filters.service';
 import { MatCardModule } from '@angular/material/card';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-catalog-header',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, NgFor],
   templateUrl: './catalog-header.component.html',
   styleUrl: './catalog-header.component.scss'
 })
