1ec334116c0791a313f2d2507e90ab91769c7abf
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-11-01 21:20:00 -0300

    Implemented header using MatExpansionPanel

--- src/app/components/catalog/catalog-header/catalog-header.component.ts
+++ src/app/components/catalog/catalog-header/catalog-header.component.ts
@@ -1,12 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
 import { FiltersService } from '../../../services/filters.service';
-import { MatCardModule } from '@angular/material/card';
 import { NgFor, NgIf } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-catalog-header',
   standalone: true,
-  imports: [MatCardModule, NgFor, NgIf],
+  imports: [MatExpansionModule, NgFor, NgIf],
   templateUrl: './catalog-header.component.html',
   styleUrl: './catalog-header.component.scss'
 })
