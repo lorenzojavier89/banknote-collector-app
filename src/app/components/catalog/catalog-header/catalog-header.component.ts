c38a0b629b4431ae688dca7ec1dc14343149b29d
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-11-02 18:17:24 -0300

    Added Filter icon to header

--- src/app/components/catalog/catalog-header/catalog-header.component.ts
+++ src/app/components/catalog/catalog-header/catalog-header.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, inject } from '@angular/core';
 import { FiltersService } from '../../../services/filters.service';
 import { NgFor, NgIf } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-catalog-header',
   standalone: true,
-  imports: [MatExpansionModule, NgFor, NgIf],
+  imports: [MatExpansionModule, MatIconModule, NgFor, NgIf],
   templateUrl: './catalog-header.component.html',
   styleUrl: './catalog-header.component.scss'
 })
