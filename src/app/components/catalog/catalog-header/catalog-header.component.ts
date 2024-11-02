d056ea10218510da4a9c74707e15cf58b18bf89c
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-11-02 19:25:12 -0300

    Added tooltip messages

--- src/app/components/catalog/catalog-header/catalog-header.component.ts
+++ src/app/components/catalog/catalog-header/catalog-header.component.ts
@@ -3,16 +3,20 @@ import { FiltersService } from '../../../services/filters.service';
 import { NgFor, NgIf } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-catalog-header',
   standalone: true,
-  imports: [MatExpansionModule, MatIconModule, NgFor, NgIf],
+  imports: [MatExpansionModule, MatIconModule, MatTooltipModule, NgFor, NgIf],
   templateUrl: './catalog-header.component.html',
   styleUrl: './catalog-header.component.scss'
 })
 export class CatalogHeaderComponent {
   private filtersService: FiltersService = inject(FiltersService);
+
+  noFiltersAppliedMssg = "Sin filtros aplicados";
+  removeAppliedFiltersMssg = "Quitar todos los filtros";
 
   filteredBanknotes = this.filtersService.filteredBanknotes;
   appliedFilter = this.filtersService.appliedFilter;
