1b00d81cba777e8e9f4679b32d430ab918b84841
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-11-09 18:36:49 -0300

    Clean up

--- src/app/components/catalog/filters/issuers-filter-panel/issuers-filter-panel.component.ts
+++ src/app/components/catalog/filters/issuers-filter-panel/issuers-filter-panel.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { FilterExpansionPanelComponent } from '../filter-expansion-panel/filter-expansion-panel.component';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FiltersService } from '../../../../services/filters.service';
 import { FilterItem } from '../../../../models/filters/filter-item.model';
 import { NgFor } from '@angular/common';
@@ -8,7 +7,7 @@ import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-issuers-filter-panel',
   standalone: true,
-  imports: [FilterExpansionPanelComponent, MatCheckboxModule, NgFor],
+  imports: [FilterExpansionPanelComponent, NgFor],
   templateUrl: './issuers-filter-panel.component.html',
   styleUrl: './issuers-filter-panel.component.scss'
 })
