64fd452904782884002eb00e440ebf8c3b85c8a1
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2024-11-18 14:59:10 -0300

    Improved styles

--- src/app/components/catalog/filters/volumes-filter-panel/volumes-filter-panel.component.ts
+++ src/app/components/catalog/filters/volumes-filter-panel/volumes-filter-panel.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { FilterExpansionPanelComponent } from '../filter-expansion-panel/filter-expansion-panel.component';
-import { NgFor } from '@angular/common';
 import { FiltersService } from '../../../../services/filters.service';
 import { FilterItem } from '../../../../models/filters/filter-item.model';
+import { Volume } from '../../../../models/volume.enum';
 
 @Component({
   selector: 'app-volumes-filter-panel',
   standalone: true,
-  imports: [FilterExpansionPanelComponent, NgFor],
+  imports: [FilterExpansionPanelComponent],
   templateUrl: './volumes-filter-panel.component.html',
   styleUrl: './volumes-filter-panel.component.scss'
 })
@@ -15,6 +15,15 @@ export class VolumesFilterPanelComponent {
   private filtersService: FiltersService = inject(FiltersService);
   
   volumes = this.filtersService.volumes;
+
+  badgeClass(code: string): string  {
+    switch (code as Volume) {
+      case Volume.Black: return 'text-bg-dark';
+      case Volume.Red: return 'text-bg-danger';
+      case Volume.Green: return 'text-bg-success';
+      default: return 'text-bg-secondary';
+    }
+  }  
 
   applyVolumeFilter(selected: boolean, volumeFilterItem: FilterItem) {
     console.log(volumeFilterItem.name);
