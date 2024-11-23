3fc13d917d3966bc761616feb2b0deea1cd8bd36
Lorenzo Javier Dominguez Yates <DominguezYat@youforce.net>
2024-11-23 13:49:41 -0300

    Added VCard Component

---
+++ src/app/components/catalog/banknote-card/banknote-vcard/banknote-vcard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banknote-vcard',
+  standalone: true,
+  imports: [],
+  templateUrl: './banknote-vcard.component.html',
+  styleUrl: './banknote-vcard.component.scss'
+})
+export class BanknoteVCardComponent {
+
+}
