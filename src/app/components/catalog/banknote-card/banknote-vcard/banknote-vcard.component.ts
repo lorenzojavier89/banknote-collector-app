858f18e914db1cda5a0110449dacef38c299c6b1
Lorenzo Javier Dominguez Yates <DominguezYat@youforce.net>
2024-11-25 12:29:51 -0300

    Added input signals (partial commit)

--- src/app/components/catalog/banknote-card/banknote-vcard/banknote-vcard.component.ts
+++ src/app/components/catalog/banknote-card/banknote-vcard/banknote-vcard.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Banknote } from '../../../../models/banknote.model';
+import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-banknote-vcard',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor],
   templateUrl: './banknote-vcard.component.html',
   styleUrl: './banknote-vcard.component.scss'
 })
 export class BanknoteVCardComponent {
-
+  banknote = input.required<Banknote>();
+  badgeClass = input.required<string>();
 }
