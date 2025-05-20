428cc583e66b50a0fa5df17b9a077201a4ffdd11
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2025-05-19 22:40:28 -0300

    Improved rarityIndexPipe styles

--- src/app/pipes/rarity-index.pipe.ts
+++ src/app/pipes/rarity-index.pipe.ts
@@ -12,22 +12,21 @@ export class RarityIndexPipe implements PipeTransform {
     let result = '';
 
     if (value >= 1 && value <= 25) {
-      result = '<div><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;""> </span></div>';
+      result = '<div style="white-space: nowrap; cursor: help;" title="Muy común"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span></div>';
     } else if (value > 25 && value <= 50) {
-      result = '<div><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;""> </span>' + 
+      result = '<div style="white-space: nowrap; cursor: help;" title="Común"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span>' + 
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;""> </span></div>';
     } else if (value > 50 && value <= 75) {
-      result = '<div><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;""> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;""> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 13px;""> </span></div>';
+      result = '<div style="white-space: nowrap; cursor: help;" title="Raro"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 13px;"> </span></div>';
     } else if (value > 75 && value <= 100) {
-      result = '<div><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;""> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;""> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 13px;""> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 14px;""> </span></div>';
+      result = '<div style="white-space: nowrap; cursor: help;" title="Muy raro"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 13px;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 14px;"> </span></div>';
     }
 
-    // const badges = Array(count).fill('<span class="badge bg-primary me-1"> </span>').join('');
     return this.domSanitizer.bypassSecurityTrustHtml(result);
   }
 
