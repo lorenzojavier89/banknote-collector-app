f2ffdf6ec2debfc50cdd09fe8b996aa9c77ecf30
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2025-05-26 20:38:35 -0300

    Improved styles

--- src/app/pipes/rarity-index.pipe.ts
+++ src/app/pipes/rarity-index.pipe.ts
@@ -12,25 +12,25 @@ export class RarityIndexPipe implements PipeTransform {
     let result = '';
 
     if (value >= 1 && value <= 25) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Muy común">' +
+      result = `<div style="white-space: nowrap; cursor: help;" title="Muy común (${value})">` +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-light" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-light" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-light" style="margin-right: 1px; font-size: 13px; border: black 1px solid;"> </span></div>';
     } else if (value > 25 && value <= 50) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Común">' +
+      result = `<div style="white-space: nowrap; cursor: help;" title="Común (${value})">` +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-light" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-light" style="margin-right: 1px; font-size: 13px; border: black 1px solid;"> </span></div>';
     } else if (value > 50 && value <= 75) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Raro">' +
+      result = `<div style="white-space: nowrap; cursor: help;" title="Raro (${value})">` +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-light" style="margin-right: 1px; font-size: 13px; border: black 1px solid;"> </span></div>';
     } else if (value > 75 && value <= 100) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Muy raro">' +
+      result = `<div style="white-space: nowrap; cursor: help;" title="Muy raro (${value})">` +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
         '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
