becd7cfcbb1ec17068946bbff08268f22809d309
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2025-05-26 20:30:43 -0300

    Improved styles

--- src/app/pipes/rarity-index.pipe.ts
+++ src/app/pipes/rarity-index.pipe.ts
@@ -12,19 +12,29 @@ export class RarityIndexPipe implements PipeTransform {
     let result = '';
 
     if (value >= 1 && value <= 25) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Muy común"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span></div>';
+      result = '<div style="white-space: nowrap; cursor: help;" title="Muy común">' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-light" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-light" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-light" style="margin-right: 1px; font-size: 13px; border: black 1px solid;"> </span></div>';
     } else if (value > 25 && value <= 50) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Común"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span>' + 
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;""> </span></div>';
+      result = '<div style="white-space: nowrap; cursor: help;" title="Común">' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-light" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-light" style="margin-right: 1px; font-size: 13px; border: black 1px solid;"> </span></div>';
     } else if (value > 50 && value <= 75) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Raro"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;"> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 13px;"> </span></div>';
+      result = '<div style="white-space: nowrap; cursor: help;" title="Raro">' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-light" style="margin-right: 1px; font-size: 13px; border: black 1px solid;"> </span></div>';
     } else if (value > 75 && value <= 100) {
-      result = '<div style="white-space: nowrap; cursor: help;" title="Muy raro"><span class="badge bg-primary" style="margin-right: 1px; font-size: 10px;"> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px;"> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 13px;"> </span>' +
-        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 14px;"> </span></div>';
+      result = '<div style="white-space: nowrap; cursor: help;" title="Muy raro">' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 9px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 10px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 11px; border: black 1px solid;"> </span>' +
+        '<span class="badge bg-primary" style="margin-right: 1px; font-size: 13px; border: black 1px solid;"> </span></div>';
     }
 
     return this.domSanitizer.bypassSecurityTrustHtml(result);
