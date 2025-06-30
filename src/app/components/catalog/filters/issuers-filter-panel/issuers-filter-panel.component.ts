9be44efaa4b00fcf4ce5219d254bcd3d346c09b5
Lorenzo Dominguez Yates <lorenzodominguezyates@gmail.com>
2025-06-29 21:01:09 -0300

    Added IssuersTooltip

--- src/app/components/catalog/filters/issuers-filter-panel/issuers-filter-panel.component.ts
+++ src/app/components/catalog/filters/issuers-filter-panel/issuers-filter-panel.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { FilterItem } from '../../../../models/filters/filter-item.model';
 import { CatalogService } from '../../../../services/catalog.service';
 import { FilterExpansionPanelComponent } from '../filter-expansion-panel/filter-expansion-panel.component';
@@ -7,7 +8,7 @@ import { FilterExpansionPanelComponent } from '../filter-expansion-panel/filter-
 @Component({
   selector: 'app-issuers-filter-panel',
   standalone: true,
-  imports: [FilterExpansionPanelComponent, NgFor],
+  imports: [FilterExpansionPanelComponent, NgFor, MatTooltipModule],
   templateUrl: './issuers-filter-panel.component.html',
   styleUrl: './issuers-filter-panel.component.scss'
 })
@@ -18,5 +19,9 @@ export class IssuersFilterPanelComponent {
 
   applyIssuerFilter(selected: boolean, issuerFilterItem: FilterItem) {
     this.catalogService.changeIssuer(selected, issuerFilterItem);
+  }
+
+  getIssuerTooltip(issuer: FilterItem): string {
+    return issuer.subItems?.map(si => si.name).join("\n") || '';
   }
 }
